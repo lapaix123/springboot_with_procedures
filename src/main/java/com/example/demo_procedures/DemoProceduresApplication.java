6071165045966e257b0593e7829b488e94bf808b
la paix <dj.lapaix.tech@gmail.com>
2024-08-13 15:49:34 +0200

    pushing project on git

---
+++ src/main/java/com/example/demo_procedures/DemoProceduresApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_procedures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProceduresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProceduresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_procedures/DemoProceduresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_procedures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProceduresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
